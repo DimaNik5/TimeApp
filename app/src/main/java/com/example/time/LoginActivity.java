e9234c671538810e0dfba8416a13e0d34e2f7f69
DimaNik5 <dima-nikiforov05@mail.ru>
2024-06-05 15:49:43 +0300

    add user

--- app/src/main/java/com/example/time/LoginActivity.java
+++ app/src/main/java/com/example/time/LoginActivity.java
@@ -2,6 +2,10 @@ package com.example.time;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
@@ -11,6 +15,8 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.example.time.user.Commands;
+import com.example.time.user.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,6 +30,10 @@ public class LoginActivity extends AppCompatActivity {
     FirebaseDatabase db;
     DatabaseReference commands;
     RelativeLayout root;
+
+    SharedPreferences pref = null;
+
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +52,68 @@ public class LoginActivity extends AppCompatActivity {
         commands = db.getReference("Commands");
 
         btnSignin.setOnClickListener((v) -> { Singin(); });
+
+        pref = getSharedPreferences(User.NAME_TABLE, Context.MODE_PRIVATE);
+
     }
 
     private void Singin() {
+        if(!inputText.getText().toString().isEmpty()){
+            inputText.setHintTextColor(Color.RED);
+            return;
+        }
+        if(inputText.getText().toString().equals("admin")){
+            Intent intent = new Intent(root.getContext(), AdminActivity.class);
+            root.getContext().startActivity(intent);
+        }
+    }
+
+    private void goToActivity(String spec){
+        Intent intent;
+        switch (spec){
+            case User.SPEC_ADMIN:
+                intent = new Intent(root.getContext(), AdminActivity.class);
+                break;
+            case User.SPEC_COMAND:
+                intent = new Intent(root.getContext(), AdminActivity.class);
+                break;
+            case User.SPEC_WAY:
+                intent = new Intent(root.getContext(), AdminActivity.class);
+                break;
+            case User.SPEC_STORE:
+                intent = new Intent(root.getContext(), AdminActivity.class);
+                break;
+            default: return;
+        }
+        root.getContext().startActivity(intent);
+    }
+
+    private void saveData(){
+        SharedPreferences.Editor editor = pref.edit();
+        //editor.putInt("namePola", 5);
+        editor.apply();
+    }
+
+    private boolean loadData(){
+        String spec = pref.getString("specId", "");
+        if(spec.isEmpty()){
+            return false;
+        }
+        switch (spec){
+            case User.SPEC_ADMIN:
+                break;
+            case User.SPEC_COMAND:
+                user = new Commands();
+                user.loadInfo(pref);
+                break;
+            case User.SPEC_WAY:
+                break;
+            case User.SPEC_STORE:
+                break;
+            default:
+                return false;
+        }
+        return true;
     }
 
     private void armAnim(){
@@ -60,5 +129,11 @@ public class LoginActivity extends AppCompatActivity {
         an.setFillAfter(true);               // keep rotation after animation
         // Aply animation to image view
         arm.setAnimation(an);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveData();
     }
 }
---
+++ app/src/main/java/com/example/time/user/User.java
@@ -0,0 +1,23 @@
+package com.example.time.user;
+
+import android.content.SharedPreferences;
+
+public class User {
+    final public static String NAME_TABLE = "user";
+    final public static String SPEC_COMAND = "com";
+    final public static String SPEC_ADMIN = "adm";
+    final public static String SPEC_WAY = "way";
+    final public static String SPEC_STORE = "str";
+
+    protected String specId;
+
+    public void createTrade(){
+
+    }
+
+    public void closeTrade(){
+
+    }
+
+    public void loadInfo(SharedPreferences pref) {}
+}
