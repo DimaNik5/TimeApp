3693f42256bf914a8468b60622c6a23a09350918
DimaNik5 <dima-nikiforov05@mail.ru>
2024-06-06 21:35:23 +0400

    add coments

--- app/src/main/java/com/example/time/LoginActivity.java
+++ app/src/main/java/com/example/time/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.time;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -24,15 +25,19 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // кнопка входа
     Button btnSignin;
+    // поле ввода логина
     TextView inputText;
+    // для работы с базой данных
     FirebaseAuth auth;
     FirebaseDatabase db;
     DatabaseReference commands;
+    // начальное поле
     RelativeLayout root;
-
+    // для хранения данных внутри памяти программы
     SharedPreferences pref = null;
-
+    // объект для реализации возможностей пользователя
     User user;
 
     @Override
@@ -43,21 +48,20 @@ public class LoginActivity extends AppCompatActivity {
         armAnim();
 
         btnSignin = findViewById(R.id.btnSignin);
+        btnSignin.setOnClickListener((v) -> { Singin(); });
         inputText = findViewById(R.id.iputText);
-
         root = findViewById(R.id.root_element);
 
         auth = FirebaseAuth.getInstance();
         db = FirebaseDatabase.getInstance();
         commands = db.getReference("Commands");
-
-        btnSignin.setOnClickListener((v) -> { Singin(); });
-
+        // выбор "области" во внутреней памяти программы
         pref = getSharedPreferences(User.NAME_TABLE, Context.MODE_PRIVATE);
-
     }
 
+    // функция входа
     private void Singin() {
+        // проверка на наличие ввода
         if(!inputText.getText().toString().isEmpty()){
             inputText.setHintTextColor(Color.RED);
             return;
@@ -68,7 +72,8 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private void goToActivity(String spec){
+    // переход на другую страницу страницу по сохранёным данным
+    private void goToActivity(@NonNull String spec){
         Intent intent;
         switch (spec){
             case User.SPEC_ADMIN:
@@ -88,16 +93,11 @@ public class LoginActivity extends AppCompatActivity {
         root.getContext().startActivity(intent);
     }
 
-    private void saveData(){
-        SharedPreferences.Editor editor = pref.edit();
-        //editor.putInt("namePola", 5);
-        editor.apply();
-    }
-
-    private boolean loadData(){
+    // загрузка данных из памяти программы и переход на соответсвенную страницу
+    private void loadData(){
         String spec = pref.getString("specId", "");
         if(spec.isEmpty()){
-            return false;
+            return;
         }
         switch (spec){
             case User.SPEC_ADMIN:
@@ -111,29 +111,36 @@ public class LoginActivity extends AppCompatActivity {
             case User.SPEC_STORE:
                 break;
             default:
-                return false;
+                return;
         }
-        return true;
+        goToActivity(spec);
     }
 
+    // установка анимации для стрелки у часов
     private void armAnim(){
-        // Locate view
+        // выбор картинки
         ImageView arm = (ImageView) findViewById(R.id.imgArm);
-        // Create an animation instance
+        // установка поворота и координат оси для анимации
         Animation an = new RotateAnimation(0.0f, 360.0f, arm.getPivotX() + 170, arm.getPivotY() + 170);
-        // Set the animation's parameters
-        an.setDuration(10000);               // duration in ms
-        an.setRepeatCount(-1);                // -1 = infinite repeated
+        // установка параметров
+        an.setDuration(10000);               // время в мс
+        an.setRepeatCount(-1);                // -1 = бесконечное повторение
         an.setRepeatMode(Animation.INFINITE);
-        an.setInterpolator(new LinearInterpolator());
-        an.setFillAfter(true);               // keep rotation after animation
-        // Aply animation to image view
+        an.setInterpolator(new LinearInterpolator()); // равномерное вращение
+        an.setFillAfter(true);               // возвращение к начальной позиции
+        // установка анимации к элементу
         arm.setAnimation(an);
     }
 
+    // сохраняет данные при выходе из приложения !!надо перенести в другие активити!!
     @Override
     protected void onDestroy() {
         super.onDestroy();
         saveData();
+    }
+    private void saveData(){
+        SharedPreferences.Editor editor = pref.edit();
+        //editor.putInt("namePola", 5);
+        editor.apply();
     }
 }
